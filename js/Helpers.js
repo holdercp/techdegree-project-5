ca81da31686ae2558794defa960dbd8fab2783c3
Chris Holder <chris@techlinksolutions.com>
2018-06-17 13:17:34 -0400

    Add capitalize function for strings

--- js/Helpers.js
+++ js/Helpers.js
@@ -1,20 +1,37 @@
 // Creates a DOM node with children
 // html can be:
+// - Blank for non-closing tags (i.e. img)
 // - A string to directly insert as innerHTML
 // - An array of child nodes to append
 // - A single DOM node to append
-function createNode(elem, html, classNames = null) {
+// Optional single calss name or array of class names
+// Optional object of attributes
+function createNode(elem, html = null, classNames = null, attrs = null) {
   const node = document.createElement(elem);
 
-  if (typeof html === 'string') {
-    node.innerHTML = html;
-  } else if (Array.isArray(html)) {
-    html.forEach(child => node.appendChild(child));
-  } else {
-    node.appendChild(html);
+  if (html) {
+    if (typeof html === 'string') {
+      node.innerHTML = html;
+    } else if (Array.isArray(html)) {
+      html.forEach(child => node.appendChild(child));
+    } else {
+      node.appendChild(html);
+    }
   }
 
-  if (classNames) classNames.forEach(className => node.classList.add(className));
+  if (classNames) {
+    if (typeof classNames === 'string') {
+      node.classList.add(classNames);
+    } else {
+      classNames.forEach(className => node.classList.add(className));
+    }
+  }
+
+  if (attrs) {
+    Object.entries(attrs).forEach((attr) => {
+      node.setAttribute(attr[0], attr[1]);
+    });
+  }
   return node;
 }
 
@@ -24,4 +41,9 @@ function removeChildNodes(parentNode) {
   while (parentNode.firstChild) {
     parentNode.removeChild(parentNode.firstChild);
   }
+}
+
+// Capitalizes string
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.substr(1);
 }
